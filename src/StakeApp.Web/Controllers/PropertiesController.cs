6938392d9582659a3e39dd71640816ffb99ad587
Logic2114 <tuoyoeyiangho@yahoo.com>
2020-09-30 16:36:49 +0100

    added html for add properties page

--- src/StakeApp.Web/Controllers/PropertiesController.cs
+++ src/StakeApp.Web/Controllers/PropertiesController.cs
@@ -1,12 +1,25 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using StakeApp.Web.Models;
 
 namespace StakeApp.Web.Controllers
 {
     public class PropertiesController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Add(PropertyModel model)
+        {
+            throw new NotImplementedException();
         }
     }
 }
